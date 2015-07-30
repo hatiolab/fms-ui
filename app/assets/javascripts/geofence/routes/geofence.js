6a135aa8c768f1c2c18ce2fc16431e4aa3db1cc9
hatiolab <shortstop@hatiolab.com>
2015-07-30 11:29:21 +0900

    back

---
+++ app/assets/javascripts/geofence/routes/geofence.js
@@ -0,0 +1,19 @@
+angular.module('fmsGeofence').config(function($stateProvider, $urlRouterProvider) {
+
+	// Each tab has its own nav history stack:
+	$stateProvider
+
+	// monitor	
+	.state('geofence', {
+		url: '/',
+		views: {
+			'sidebar-view': {
+				templateUrl: '/assets/geofence/views/sidebar.html'
+			},
+			'content-view' : {
+				templateUrl: '/assets/geofence/views/geofence.html'
+			}
+		}
+	});
+
+});
