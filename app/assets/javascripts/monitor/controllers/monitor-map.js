3b63064b03f620a5741c3cd08cdda93b8cc2fa4e
hatiolab <shortstop@hatiolab.com>
2015-08-14 17:07:16 +0900

    debuggin

--- app/assets/javascripts/monitor/controllers/monitor-map.js
+++ app/assets/javascripts/monitor/controllers/monitor-map.js
@@ -271,27 +271,27 @@ angular.module('fmsMonitor').controller('MonitorMapCtrl', function($rootScope, $
 				stroke : { color: '#FF0000', opacity: 1.0, weight: 4 }
 			};
 
-			//$scope.polylines.push(batchline);
+			$scope.polylines.push(batchline);
 
 			// 2.3 tracks
 			for(var j = 0 ; j < tracks.length ; j++) {
 				if(tracks[j].bid == batch.id) {
-					//$scope.addMarker($scope.trackToMarker(tracks[j]));
-					//batchline.path.push({latitude : tracks[j].lat, longitude : tracks[j].lng});
+					$scope.addMarker($scope.trackToMarker(tracks[j]));
+					batchline.path.push({latitude : tracks[j].lat, longitude : tracks[j].lng});
 				}
 			}
 
 			// 2.4 events
 			for(var k = 0 ; k < events.length ; k++) {
 				//if(events[k].bid == batch.id && events[k].typ != 'V') {
-				$scope.addMarker($scope.eventToMarker(events[k]));
+				//$scope.addMarker($scope.eventToMarker(events[k]));
 				//}
 			}
 
 			// 2.5 batch end
 			if(batch.sts == '2') {
 				$scope.addMarker($scope.batchToMarker(batch, 'end'));
-				//batchline.path.push({latitude : batch.lat, longitude : batch.lng});
+				batchline.path.push({latitude : batch.lat, longitude : batch.lng});
 			}
 		}
 
