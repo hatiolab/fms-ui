d0bf276e602fa87aca1c4eb32a940aede171b681
heartyoh <heartyoh@hatiolab.com>
2015-08-30 01:40:44 +0900

    pip-video

--- app/assets/javascripts/monitor/directives/pip-video.js
+++ app/assets/javascripts/monitor/directives/pip-video.js
@@ -1,51 +1,5 @@
 angular.module('pip', [])
-    .directive('pipVideo', function() {
-        return {
-            restrict: 'E',
-            scope: {},
-            controller: 'pipVideoCtrl',
-            link: function(scope, e, a) {
-                scope.videoUrl = a.videoUrl;
-                scope.frontVideoUrl = a.frontVideoUrl;
-                scope.rearVideoUrl = a.rearVideoUrl;
-                scope.audioUrl = a.audioUrl;
-            },
-            template: '' +
-                '<div class="pip-container" ng-if="frontVideoUrl != null && frontVideoUrl != \'\'">' +
-                '   <video ng-attr-src="{{ frontVideoUrl }}" controls xmediagroup="pip" class="backward-layer"></video>' +
-                '   <video ng-attr-src="{{ rearVideoUrl }}" xmediagroup="pip" class="forward-layer"></video>' +
-                '   <audio ng-attr-src="{{ audioUrl }}" xmediagroup="pip" hidden></audio>' +
-                '</div>' +
-                '<div ng-if="frontVideoUrl">' +
-                    '<a target="_self" href="{{ videoUrl }}" download="video.mp4">' +
-                        '[Download]' +
-                    '</a>' +
-                '<div>'
-        };
-    })
-    .directive('pipImage', function() {
-        return {
-            restrict: 'E',
-            scope: {},
-            controller: 'pipImageCtrl',
-            link: function(scope, e, a) {
-                scope.frontImageUrl = a.frontImageUrl;
-                scope.rearImageUrl = a.rearImageUrl;
-            },
-            template: '' +
-                '<div class="pip-container" ng-if="frontImageUrl != null && frontImageUrl != \'\'">' +
-                '   <img ng-attr-src="{{ frontImageUrl }}" class="backward-layer"></img>' +
-                '   <img ng-attr-src="{{ rearImageUrl }}" class="forward-layer"></img>' +
-                '</div>'
-        };
-    })
-    .controller('pipImageCtrl', function($scope, $element, $attrs) {
-        $element.on('click', '.pip-container img.forward-layer', function(){
-            $(".backward-layer").removeClass("backward-layer").addClass("forward-layer");
-            $(this).addClass("backward-layer").removeClass("forward-layer");
-        })
-    })
-    .controller('pipVideoCtrl', function($scope, $element, $attrs) {
+    .run(function() {
         $.createEventCapturing = (function () {
             var special = $.event.special;
             return function (names) {
@@ -80,53 +34,103 @@ angular.module('pip', [])
 
         $.createEventCapturing(['canplay', 'play', 'pause', 'seeked']);
 
-        $element.on('click', '.pip-container video.forward-layer', function(e){
-            var video = e.target;
+    })
+    .directive('pipVideo', function() {
 
-            var mediagroup = $(video).attr('xmediagroup');
-            $('video[xmediagroup=' + mediagroup + ']').not(video).each(function(){
-                $(this).prop('controls', false);
-                $(this).removeClass("backward-layer").addClass("forward-layer");
-            });
+        return {
+            restrict: 'E',
+            scope: {},
+            controller: 'pipVideoCtrl',
+            link: function(scope, $element, a) {
 
-            $(video).prop('controls', true);
-            $(this).addClass("backward-layer").removeClass("forward-layer");
-        });
+                scope.videoUrl = a.videoUrl;
 
-        $element.on('play', 'video.backward-layer', function(e) {
-            var video = e.target;
+                scope.frontVideoUrl = a.frontVideoUrl;
+                scope.rearVideoUrl = a.rearVideoUrl;
+                scope.audioUrl = a.audioUrl;
 
-            var mediagroup = $(video).attr('xmediagroup');
-            $('video,audio[xmediagroup=' + mediagroup + ']').not(video).each(function(){
-                this.currentTime = video.currentTime;
-                this.play();
-            });
-        });
+                $element.on('click', '.pip-container video.forward-layer', function(e){
+                    var video = e.target;
 
-        $element.on('pause', 'video.backward-layer', function(e) {
-            var video = e.target;
+                    var mediagroup = $(video).attr('xmediagroup');
+                    $('video[xmediagroup=' + mediagroup + ']').not(video).each(function(){
+                        $(this).prop('controls', false);
+                        $(this).removeClass("backward-layer").addClass("forward-layer");
+                    });
 
-            var mediagroup = $(video).attr('xmediagroup');
-            $('video,audio[xmediagroup=' + mediagroup + ']').not(video).each(function(){
-                this.pause();
-                this.currentTime = video.currentTime;
-            });
-        });
+                    $(video).prop('controls', true);
+                    $(this).addClass("backward-layer").removeClass("forward-layer");
+                });
 
-        $element.on('seeked', 'video.backward-layer', function(e) {
-            var video = e.target;
+                $element.on('play', 'video.backward-layer', function(e) {
+                    var video = e.target;
 
-            var mediagroup = $(video).attr('xmediagroup');
-            $('video,audio[xmediagroup=' + mediagroup + ']').not(video).each(function(){
-                this.currentTime = video.currentTime;
-            });
-        });
+                    var mediagroup = $(video).attr('xmediagroup');
+                    $('video,audio[xmediagroup=' + mediagroup + ']').not(video).each(function(){
+                        this.currentTime = video.currentTime;
+                        this.play();
+                    });
+                });
 
-        $element.on('canplay', 'video.backward-layer', function(e) {
-            $element.find('video.backward-layer').trigger('play');
-        });
+                $element.on('pause', 'video.backward-layer', function(e) {
+                    var video = e.target;
 
-        // $scope.$watch("toggle", function(value) {
-        //     element.toggleClass('active', value)
-        // })
+                    var mediagroup = $(video).attr('xmediagroup');
+                    $('video,audio[xmediagroup=' + mediagroup + ']').not(video).each(function(){
+                        this.pause();
+                        this.currentTime = video.currentTime;
+                    });
+                });
+
+                $element.on('seeked', 'video.backward-layer', function(e) {
+                    var video = e.target;
+
+                    var mediagroup = $(video).attr('xmediagroup');
+                    $('video,audio[xmediagroup=' + mediagroup + ']').not(video).each(function(){
+                        this.currentTime = video.currentTime;
+                    });
+                });
+
+                $element.on('canplay', 'video.backward-layer', function(e) {
+                    $element.find('video.backward-layer').trigger('play');
+                });                
+
+            },
+            template: '' +
+                '<div class="pip-container" ng-if="frontVideoUrl != null && frontVideoUrl != \'\'">' +
+                '   <video ng-attr-src="{{ frontVideoUrl }}" controls xmediagroup="pip" class="backward-layer"></video>' +
+                '   <video ng-attr-src="{{ rearVideoUrl }}" xmediagroup="pip" class="forward-layer"></video>' +
+                '   <audio ng-attr-src="{{ audioUrl }}" xmediagroup="pip" hidden></audio>' +
+                '</div>' +
+                '<div ng-if="frontVideoUrl">' +
+                    '<a target="_self" href="{{ videoUrl }}" download="video.mp4">' +
+                        '[Download]' +
+                    '</a>' +
+                '<div>'
+        };
+    })
+    .directive('pipImage', function() {
+        return {
+            restrict: 'E',
+            scope: {},
+            controller: 'pipImageCtrl',
+            link: function(scope, $element, a) {
+                scope.frontImageUrl = a.frontImageUrl;
+                scope.rearImageUrl = a.rearImageUrl;
+
+                $element.on('click', '.pip-container img.forward-layer', function(){
+                    $(".backward-layer").removeClass("backward-layer").addClass("forward-layer");
+                    $(this).addClass("backward-layer").removeClass("forward-layer");
+                })
+            },
+            template: '' +
+                '<div class="pip-container" ng-if="frontImageUrl != null && frontImageUrl != \'\'">' +
+                '   <img ng-attr-src="{{ frontImageUrl }}" class="backward-layer"></img>' +
+                '   <img ng-attr-src="{{ rearImageUrl }}" class="forward-layer"></img>' +
+                '</div>'
+        };
+    })
+    .controller('pipImageCtrl', function($scope, $element, $attrs) {
+    })
+    .controller('pipVideoCtrl', function($scope, $element, $attrs) {
     });
