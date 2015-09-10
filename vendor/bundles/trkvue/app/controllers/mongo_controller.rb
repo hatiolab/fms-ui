e173e3751ae313d1a52de2f1dbacdad5aac1f672
hatiolab <shortstop@hatiolab.com>
2015-09-10 20:00:51 +0900

    Debugging

--- vendor/bundles/trkvue/app/controllers/mongo_controller.rb
+++ vendor/bundles/trkvue/app/controllers/mongo_controller.rb
@@ -59,29 +59,31 @@ class MongoController < DomainAppController
   end
 
   def to_from_times(dateStr)
-    # ori_timezone = Time.zone
-    # Time.zone = Domain.current_domain.timezone
+    ori_timezone = Time.zone
+    Time.zone = Domain.current_domain.timezone
+    times = Time.zone.parse(dateStr).to_time.to_i * 1000
+    debug_print "Time zone : #{Time.zone}, From Date : #{dateStr}, Value : #{times}"
+    Time.zone = ori_timezone
+    return times
+    
     # times = Date.parse(dateStr).to_time.to_i * 1000
     # debug_print "Time zone : #{Time.zone}, From Date : #{dateStr}, Value : #{times}"
-    # Time.zone = ori_timezone
     # return times
-    times = Date.parse(dateStr).to_time.to_i * 1000
-    debug_print "Time zone : #{Time.zone}, From Date : #{dateStr}, Value : #{times}"
-    return times
   end
 
   def to_to_times(dateStr)
-    # ori_timezone = Time.zone
-    # toDateStr = (Date.parse(dateStr) + 1).strftime('%Y-%m-%d')
-    # Time.zone = Domain.current_domain.timezone
-    # times = Date.parse(toDateStr).to_time.to_i * 1000
-    # debug_print "Time zone : #{Time.zone}, To Date : #{dateStr}, Value : #{times}"
-    # Time.zone = ori_timezone
-    # return times
+    ori_timezone = Time.zone
     toDateStr = (Date.parse(dateStr) + 1).strftime('%Y-%m-%d')
-    times = Date.parse(toDateStr).to_time.to_i * 1000
+    Time.zone = Domain.current_domain.timezone
+    times = Time.zone(toDateStr).to_time.to_i * 1000
     debug_print "Time zone : #{Time.zone}, To Date : #{toDateStr}, Value : #{times}"
+    Time.zone = ori_timezone
     return times
+    
+    # toDateStr = (Date.parse(dateStr) + 1).strftime('%Y-%m-%d')
+    # times = Date.parse(toDateStr).to_time.to_i * 1000
+    # debug_print "Time zone : #{Time.zone}, To Date : #{toDateStr}, Value : #{times}"
+    # return times
   end  
 
   #
