23e247844fd02b4e7cf98ab94f42f5fe04beb119
Lyonghwan Park <longhuan@hatiolab.com>
2015-09-14 21:17:29 +0900

    Fix Trip, Batch End Time, Hourly Summary

--- vendor/bundles/trkvue/app/models/summary.rb
+++ vendor/bundles/trkvue/app/models/summary.rb
@@ -17,6 +17,8 @@ class Summary
 				endTime = Summary.hourly_summary_by_domain(domain, now)
 				domain.last_summary_time = endTime / 1000
 				domain.save
+			else
+				debug_print "Domain [#{domain.name}] has no timezone, so skip summary!"
 			end
 		end
 		
@@ -28,13 +30,14 @@ class Summary
 	#
 	def self.hourly_summary_by_domain(domain, now)
 		# 1. 날짜, 년, 월, 일, 주, 시작 시간, 끝 시간 값을 구한다.
-  	date_info = Summary.get_hourly_summary_time_info(domain, now)
-  	sum_date, year, month, week, startTime, endTime = date_info[0], date_info[1], date_info[2], date_info[3], date_info[4], date_info[5]
-    # 2. Current Domain 설정 
-    Domain.current_domain = domain
-    debug_print "Domain (#{domain.name}) houly summary start..."
+  		date_info = Summary.get_hourly_summary_time_info(domain, now)
+  		sum_date, year, month, week, startTime, endTime = date_info[0], date_info[1], date_info[2], date_info[3], date_info[4], date_info[5]
+    	# 2. Current Domain 설정 
+    	Domain.current_domain = domain
+    	debug_print "Domain (#{domain.name}) houly summary start..."
 		# 4. Fleet 별 Daily Summary
-		Summary.do_fleet_summary(domain, sum_date, year, month, week, startTime, endTime)
+		closingDailySummary = (now.hour == 0);	# 0시이면 이전 날짜에 대한 서머리를 종료한다.
+		Summary.do_fleet_summary_by_hourly(domain, sum_date, year, month, week, startTime, endTime, closingDailySummary)
 		# 5. Fleet Group 별 Daily Summary
 		Summary.do_fleet_group_summary(domain, sum_date, year, month, week)
 		debug_print "Domain (#{domain.name}) hourly summary completed"
@@ -48,12 +51,12 @@ class Summary
 		# 1. 타임존에 따른 현재 시각을 구한다.
 		sum_date, date_format = ((now.hour == 0) ? sum_date - 1 : now.to_date), '%Y-%m-%d'
 		# 2. 날짜, 년, 월, 일, 주 값을 구한다.
-    year, month, week = sum_date.year, sum_date.month, sum_date.strftime("%U").to_i
+    	year, month, week = sum_date.year, sum_date.month, sum_date.strftime("%U").to_i
 
-    sumDateStr = sum_date.strftime(date_format)
-    #startTime = ActiveSupport::TimeZone[Domain.current_domain.timezone].parse(sumDateStr).to_time.to_i * 1000
-    startTime = (domain.last_summary_time * 1000) + 1
-    endTime = now.to_time.to_i * 1000
+    	sumDateStr = sum_date.strftime(date_format)
+    	startTime = ActiveSupport::TimeZone[Domain.current_domain.timezone].parse(sumDateStr).to_time.to_i * 1000
+    	#startTime = (domain.last_summary_time * 1000) + 1
+    	endTime = now.to_time.to_i * 1000
 
 		if(now.hour == 0)
 			# 시각이 0시 이면 서머리 날짜는 전날이고 서머리 완료시간이 정각 자정이어야 한다.
@@ -64,6 +67,70 @@ class Summary
 		
 		return [sum_date, year, month, week, startTime, endTime]
 	end
+
+	#
+	# Do Hourly Summary
+	#
+	def self.do_fleet_summary_by_hourly(domain, date, year, month, week, startTime, endTime)
+		fleets = Fleet.all
+		return unless fleets
+		
+		trips = Summary.find_trips(domain, startTime, endTime)
+
+		fleets.each do |fleet|
+			debug_print fleet.name
+			Summary.summary_hourly_by_fleet(domain, startTime, endTime, date, year, month, week, fleet, trips)
+		end
+	end
+
+	#
+	# Hourly Summary By Fleet
+	#
+	def self.summary_hourly_by_fleet(domain, startTime, endTime, date, year, month, week, fleet, trips, closingDailySummary)
+		track_conds = { 'stm' => { '$gte' => startTime , '$lt' => endTime }, 'fid' => fleet.name }
+		event_conds = { 'etm' => track_conds['stm'], 'fid' => track_conds['fid'] }
+
+		fleetSum = FleetSummary.where("sum_day = ? and fleet_id = ?", date.strftime('%Y-%m-%d'), fleet.id).first
+		unless fleetSum
+			fleetSum = FleetSummary.new 
+			fleetSum.fleet_id = fleet.id
+			fleetSum.sum_day = date.strftime('%Y-%m-%d')
+			fleetSum.sum_year = year
+			fleetSum.sum_month = month
+			fleetSum.sum_week = week
+		end
+
+		fleetSum.driver_id = fleet.driver_id
+		fleetSum.drive_time = Summary.get_trip_time(trips, fleet, startTime, endTime)
+		fleetSum.drive_dist = Summary.get_track_sum(track_conds, 'dst')
+		fleetSum.impact = Summary.get_event_count(event_conds, 'G')
+		fleetSum.geofence = Summary.get_event_count(event_conds, ['I', 'O'])
+		fleetSum.emergency = Summary.get_event_count(event_conds, 'B')
+		fleetSum.overspeed = Summary.get_event_count(event_conds, 'V')
+		fleetSum.speed_off = Summary.get_speed_count(track_conds, 'c_off', 'speed_off')
+		fleetSum.speed_idle = Summary.get_speed_count(track_conds, 'c_idl', 'speed_idle')
+		fleetSum.speed_slow = Summary.get_speed_count(track_conds, 'c_low', 'speed_slow')
+		fleetSum.speed_normal = Summary.get_speed_count(track_conds, 'c_nml', 'speed_normal')
+		fleetSum.speed_high = Summary.get_speed_count(track_conds, 'c_hgh', 'speed_high')
+		fleetSum.speed_over = Summary.get_speed_count(track_conds, 'c_ovr', 'speed_over')
+		fleetSum.velocity = Summary.get_track_avg(track_conds, 'vlc')
+		fleetSum.save!
+
+		if(closingDailySummary)
+			# fleet, driver -> Total drive_time, drive_dist 정보를 계산하여 fleet, driver에 설정 
+			fleet.drive_time += fleetSum.drive_time
+			fleet.drive_dist += fleetSum.drive_dist
+			fleet.save!
+
+			if(fleet.driver)
+				driver = fleet.driver
+				driver.work_time += fleetSum.drive_time
+				driver.distance += fleetSum.drive_dist
+				driver.point += fleetSum.speed_over
+				driver.save!
+			end
+		end
+	end	
 
 	############################################################################################
 	# 																	Daily Summary
@@ -88,11 +155,11 @@ class Summary
 		# 1. 서머리 시간 체크 
 		return if(!Summary.check_summary_time(domain))
 		# 2. 날짜, 년, 월, 일, 주 값을 구한다.
-  	date_info = Summary.get_summary_date_info(domain, ActiveSupport::TimeZone[domain.timezone].now)
-  	sum_date, year, month, week, startTime, endTime = date_info[0], date_info[1], date_info[2], date_info[3], date_info[4], date_info[5]
-    # 3. Current Domain 설정 
-    Domain.current_domain = domain
-    debug_print "Domain (#{domain.name}) daily summary start..."
+  		date_info = Summary.get_summary_date_info(domain, ActiveSupport::TimeZone[domain.timezone].now)
+  		sum_date, year, month, week, startTime, endTime = date_info[0], date_info[1], date_info[2], date_info[3], date_info[4], date_info[5]
+    	# 3. Current Domain 설정 
+    	Domain.current_domain = domain
+    	debug_print "Domain (#{domain.name}) daily summary start..."
 		# 4. Fleet 별 Daily Summary
 		Summary.do_fleet_summary(domain, sum_date, year, month, week, startTime, endTime)
 		# 5. Fleet Group 별 Daily Summary
@@ -115,10 +182,10 @@ class Summary
 		# 2. 타임존에 따른 현재 시각을 구한다.
 		date_format, sum_date = '%Y-%m-%d', now.to_date - 1
 		# 3. 날짜, 년, 월, 일, 주 값을 구한다.
-    year, month, week = sum_date.year, sum_date.month, sum_date.strftime("%U").to_i
-    # 4. 시작시간 타임스탬프, 완료시간 타임스탬프를 구한다.
-    startTime = ActiveSupport::TimeZone[domain.timezone].parse(sum_date.strftime(date_format)).to_time.to_i * 1000
-    endTime = ActiveSupport::TimeZone[domain.timezone].parse(now.strftime(date_format)).to_time.to_i * 1000
+    	year, month, week = sum_date.year, sum_date.month, sum_date.strftime("%U").to_i
+    	# 4. 시작시간 타임스탬프, 완료시간 타임스탬프를 구한다.
+    	startTime = ActiveSupport::TimeZone[domain.timezone].parse(sum_date.strftime(date_format)).to_time.to_i * 1000
+    	endTime = ActiveSupport::TimeZone[domain.timezone].parse(now.strftime(date_format)).to_time.to_i * 1000
 		[sum_date, year, month, week, startTime, endTime]		
 	end
 
@@ -145,14 +212,14 @@ class Summary
 		# 2. 타임존에 따른 현재 시각을 구한다.
 		now, date_format = ActiveSupport::TimeZone[domain.timezone].parse(dateStr).to_time, '%Y-%m-%d'
 		# 3. 날짜, 년, 월, 일, 주 값을 구한다.
-  	sum_date = now.to_date
-    year, month, week = sum_date.year, sum_date.month, sum_date.strftime("%U").to_i
-    # 4. 시작시간 타임스탬프, 완료시간 타임스탬프를 구한다.
-    startTime = ActiveSupport::TimeZone[domain.timezone].parse(sum_date.strftime(date_format)).to_time.to_i * 1000
-    endTime = ActiveSupport::TimeZone[domain.timezone].parse((sum_date + 1).strftime(date_format)).to_time.to_i * 1000
-    # 5. Current Domain 설정 
-    Domain.current_domain = domain
-    debug_print "Domain (#{domain.name}) daily summary start..."
+  		sum_date = now.to_date
+    	year, month, week = sum_date.year, sum_date.month, sum_date.strftime("%U").to_i
+    	# 4. 시작시간 타임스탬프, 완료시간 타임스탬프를 구한다.
+    	startTime = ActiveSupport::TimeZone[domain.timezone].parse(sum_date.strftime(date_format)).to_time.to_i * 1000
+    	endTime = ActiveSupport::TimeZone[domain.timezone].parse((sum_date + 1).strftime(date_format)).to_time.to_i * 1000
+    	# 5. Current Domain 설정 
+    	Domain.current_domain = domain
+    	debug_print "Domain (#{domain.name}) daily summary start..."
 		# 6. Fleet 별 Daily Summary
 		Summary.do_fleet_summary(domain, sum_date, year, month, week, startTime, endTime)
 		# 7. Fleet Group 별 Daily Summary
@@ -174,7 +241,7 @@ class Summary
 		trips = Summary.find_trips(domain, startTime, endTime)
 
 		fleets.each do |fleet|
-			debug_print fleet
+			debug_print fleet.name
 			Summary.summary_by_fleet(domain, startTime, endTime, date, year, month, week, fleet, trips)
 		end
 	end
@@ -205,7 +272,6 @@ class Summary
 
 		fleetSum.driver_id = fleet.driver_id
 		fleetSum.drive_time = Summary.get_trip_time(trips, fleet, startTime, endTime)
-		fleetSum.velocity = Summary.get_track_avg(track_conds, 'vlc')
 		fleetSum.drive_dist = Summary.get_track_sum(track_conds, 'dst')
 		fleetSum.impact = Summary.get_event_count(event_conds, 'G')
 		fleetSum.geofence = Summary.get_event_count(event_conds, ['I', 'O'])
@@ -217,6 +283,7 @@ class Summary
 		fleetSum.speed_normal = Summary.get_speed_count(track_conds, 'c_nml', 'speed_normal')
 		fleetSum.speed_high = Summary.get_speed_count(track_conds, 'c_hgh', 'speed_high')
 		fleetSum.speed_over = Summary.get_speed_count(track_conds, 'c_ovr', 'speed_over')
+		fleetSum.velocity = Summary.get_track_avg(track_conds, 'vlc')
 		fleetSum.save!
 
 		# fleet, driver -> Total drive_time, drive_dist 정보를 계산하여 fleet, driver에 설정 
@@ -262,9 +329,10 @@ class Summary
 	end
 
 	#
-	# Track의 선택 필드에 대한 평균값 계산 
+	# Track의 선택 필드에 대한 평균값 계산, 속도가 0인 track은 제외한다.
 	#
 	def self.get_track_avg(conds, field)
+		conds['vlc'] = { "$gt" : 0 }
 		match = { "$match" => conds }
 		group = { "$group" => { "_id" => nil, "avg" => { "$avg" => "$#{field}" }} }
 		result = Track.collection.aggregate([match, group])
@@ -357,7 +425,7 @@ class Summary
 			groupSum.sum_week = week
 		end
 
-		groupSum.velocity = 		Summary.avg_by_group(fleetIds, groupSum.sum_day, 'velocity')
+		groupSum.velocity = 	Summary.avg_by_group(fleetIds, groupSum.sum_day, 'velocity')
 		groupSum.drive_time =   Summary.sum_by_group(fleetIds, groupSum.sum_day, 'drive_time')
 		groupSum.drive_dist =   Summary.sum_by_group(fleetIds, groupSum.sum_day, 'drive_dist')
 		groupSum.impact =       Summary.sum_by_group(fleetIds, groupSum.sum_day, 'impact')
